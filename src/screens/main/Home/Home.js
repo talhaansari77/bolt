242fdacec8cf668ed77701d723bb74ddaf92f35a
darkpixels201 <darkpixels201@gmail.com>
2022-12-18 23:26:07 +0500

    MapInstall

--- src/screens/main/Home/Home.js
+++ src/screens/main/Home/Home.js
@@ -1,14 +1,23 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import MapView from 'react-native-maps';
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <View style={styles.container}>
+    <MapView style={styles.map} />
+  </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
